4b58a14b84b2fd8735f7d9b3f59fbc7346b72628
naslanzade <novrastafa@code.edu.az>
2023-10-27 20:02:00 +0400

    adding tab-menu logics to home page

--- assets/js/home.js
+++ assets/js/home.js
@@ -14,5 +14,60 @@ $(document).ready(function () {
     sidebar.classList.remove("active-bar");
   });
 
-  
+  //Events Tab-Menu
+  let tabMenu = document.querySelectorAll("#events .tab-menu .tab");
+  let menu = document.querySelectorAll("#events .menu");
+  tabMenu.forEach((eachTabMenu) => {
+    eachTabMenu.addEventListener("click", function (e) {
+      document.querySelector(".active-tab").classList.remove("active-tab");
+      this.classList.add("active-tab");
+
+      menu.forEach((eachMenu) => {
+        if (this.getAttribute("data-id") == eachMenu.getAttribute("data-id")) {
+          eachMenu.classList.remove("d-none");
+        } else {
+          eachMenu.classList.add("d-none");
+        }
+      });
+    });
+  });
+
+
+
+  //Menu Tab-Menu
+  let foodTab=document.querySelectorAll("#menu .tab-menu .tab");
+  let food =document.querySelectorAll("#menu .food");
+
+  foodTab.forEach((eachTabFood) => {
+    eachTabFood.addEventListener("click", function (e) {
+      document.querySelector(".active-tab").classList.remove("active-tab");
+      this.classList.add("active-tab");
+
+      food.forEach((eachFood) => {
+        if (this.getAttribute("data-id") == eachFood.getAttribute("data-id")) {
+          eachFood.classList.remove("d-none");
+        } else {
+          eachFood.classList.add("d-none");
+        }
+      });
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
